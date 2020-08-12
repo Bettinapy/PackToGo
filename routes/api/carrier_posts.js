17bd594ea6d0583a769bf1b4c003c3d8d43b97c7
geo5745 <g.rodnikov@gmail.com>
2020-08-11 20:54:21 -0500

    post-carrier-model routes complete

--- routes/api/carrier_posts.js
+++ routes/api/carrier_posts.js
@@ -47,24 +47,81 @@ router.post('/create', passport.authenticate('jwt', { session: false }), (req,re
 
 router.put('/:id', passport.authenticate('jwt', { session: false }), (req,res) => {
 
-
-
-    CarrierPost.findById(req.params.id)
-        .then(post => {
-            
-            if (post.carrierId.toString() !== req.user.id.toString()) {
-                return res.status(401).json({user: "Only the creator of the post can edit it"})
-            }
-        })
-        .catch(() => res.status(404).json({error: "Post not found"}))
-
     const { errors, isValid } = validateCarrierPostUpdate(req.body);
     if (!isValid) {
         return res.status(400).json(errors);
     }
 
-    CarrierPost.findByIdAndUpdate(req.params.id, req.body, {new:true})
-        .then(post => res.json(post))
+    CarrierPost.findById(req.params.id)
+        .then(post => {
+            if (post.carrierId.toString() !== req.user.id.toString()) {
+                return res.status(401).json({user: "Only the creator of the post can edit it"})
+            } else {
+                CarrierPost.findByIdAndUpdate(req.params.id, req.body, {new:true})
+                    .then(post => res.json(post))
+                    .catch(err => res.json(err))
+            }
+        })
+        .catch(() => res.status(404).json({error: "Post not found"}))
+});
+
+router.get('/', (req, res) => {
+    const allPosts = {};
+
+    CarrierPost.find()
+        .then(posts => {
+            posts.forEach(post => {
+                allPosts[post.id] = post;
+            })
+            res.json(allPosts)
+        })
+        .catch(err => res.json(err))
+});
+
+router.get('/:id', (req,res) => {
+    const onePost = {};
+
+    CarrierPost.findById(req.params.id)
+        .then(post => {
+            onePost[post.id] = post;
+            res.json(onePost)})
+        .catch(err => res.json(err))
+});
+
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req,res) => {
+
+    CarrierPost.findById(req.params.id)
+        .then(post => {
+            if (post.carrierId.toString() !== req.user.id.toString()) {
+                return res.status(401).json({user: "Only the creator of the post can edit it"})
+            } else {
+                CarrierPost.findByIdAndDelete(req.params.id)
+                    .then(post => res.json({}))
+                    .catch(err => res.json(err))
+            }
+        })
+        .catch(() => res.status(404).json({error: "Post not found"}))
+
+});
+
+router.get('/user/:id', (req,res) => {
+    const allUsersPosts = {};
+    let selectedUser;
+    
+    User.findById(req.params.id)
+        .then(user => {
+            selectedUser = user;
+        })
+    
+    //console.log(selectedUser.handle);
+    
+    CarrierPost.find({carrierId: selectedUser})
+        .then(posts => {
+            posts.forEach(post => {
+                allPosts[post.id] = post;
+            })
+            res.json(allPosts)
+        })
         .catch(err => res.json(err))
 });
 
